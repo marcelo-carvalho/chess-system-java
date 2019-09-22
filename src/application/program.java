211cb3d2713b5ed54b6730e30ef689179d7760f1
Marcelo Carvalho <mdecarvalhopereira@nevada.unr.edu>
2019-09-22 18:14:38 -0300

    implementação de Chess layer e apresentação do tabuleiro

--- src/application/program.java
+++ src/application/program.java
@@ -1,13 +1,18 @@
 package application;
 
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class program {
 
   public static void main(String[] args) {
     
-    Position pos = new Position(3, 5);
-    System.out.println(pos);
+   
+    ChessMatch chessMatch = new ChessMatch();
+    UI.printBoard(chessMatch.getPieces());
+    
+    
+    
 
   }
 
--- src/boradgame/Board.java
+++ src/boradgame/Board.java
@@ -1,11 +1,43 @@
 package boradgame;
 
+import boardgame.Piece;
+import boardgame.Position;
+
 public class Board {
   
   private int rows;
   private int columns;
   private Piece[][] pieces;
+ 
   
+  public Board(int rows, int columns) {
+    this.rows = rows;
+    this.columns = columns;
+    pieces = new Piece[rows][columns];
+  }
+
+  public int getRows() {
+    return rows;
+  }
+
+  public void setRows(int rows) {
+    this.rows = rows;
+  }
+
+  public int getColumns() {
+    return columns;
+  }
+
+  public void setColumns(int columns) {
+    this.columns = columns;
+  }
   
+  public Piece piece(int row, int column) {
+    return pieces[row][column];
+  }
+  
+  public Piece piece(Position position) {
+    return pieces[position.getRow()][position.getColumn()];
+  }
 
 }
